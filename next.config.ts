773356e89a658ceb911ab0042a74d6bedc063049
ai-chemist <utk8244@gmail.com>
2025-12-18 17:08:46 +0900

    next.config.ts updateed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '10mb', // 👈 여기! 제한을 10MB로 늘림
+        },
+    },
 };
 
 export default nextConfig;
